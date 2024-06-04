182d764ee380ebb148a204f324f90b88afc246fa
aboodmokied <78992620+aboodmokied@users.noreply.github.com>
2024-06-04 11:07:51 +0300

    Express Server Setup

--- app/app.js
+++ app/app.js
@@ -1,1 +1,13 @@
+const express=require('express');
 require('dotenv').config();
+const app=express();
+
+
+// Routes
+// ... 
+
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server Running on PORT: ${PORT}`);
+})
