5a147483bae681e8f79ae4dc93a871d62678fbdf
aboodmokied <78992620+aboodmokied@users.noreply.github.com>
2024-08-05 20:19:40 +0300

    implement sorting as query feature

--- app/util/QueryFeatures.js
+++ app/util/QueryFeatures.js
@@ -11,14 +11,42 @@ class QueryFeatures{
         excludedParmas.forEach(ele=>{
             delete filteringProps[ele];
         });
-        let filteringPropsAsStr=JSON.stringify(filteringProps);
-        filteringPropsAsStr.replace(/\b(ne|gt|gte|lt|lte)\b/g,(match)=>`[Op.${match}]`);
-        const where=JSON.parse(filteringPropsAsStr);
-        queryOptions.where=where;
+
+        if(Object.keys(filteringProps).length){
+            let filteringPropsAsStr=JSON.stringify(filteringProps);
+            filteringPropsAsStr.replace(/\b(ne|gt|gte|lt|lte)\b/g,(match)=>`[Op.${match}]`);
+            const where=JSON.parse(filteringPropsAsStr);
+            this.queryOptions.where=where;
+        }
+        
         return this;
     }
+
     // sort
-    
+    sort(){
+        /**
+         * sort:'-price,age'
+         * [['price','DESC'],['age','ASC']]
+         * */ 
+        if(this.queryStr.sort){
+            const sortingProps=this.queryStr.sort.split(',');
+            const propsArr=[];
+            sortingProps.forEach(prop=>{
+                let ord=null;
+                let filed=prop;
+                if(prop.startsWith('-')){
+                    ord='DESC';
+                    filed=prop.substring(1);
+                }else{
+                    ord='ASC';
+                }
+                propsArr.push([filed,ord]);
+            });
+            this.queryOptions.order=propsArr;
+        }
+        return this;
+    }
+
     // limiting fileds
 
     // pagination
